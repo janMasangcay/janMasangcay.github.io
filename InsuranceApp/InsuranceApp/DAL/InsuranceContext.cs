bfd98781a0d89f5450eafdf02540458574b72f56
JanCaesar <janCaesar@github.com>
2019-05-18 23:37:17 -0700

    Creating InsuranceContext.

---
+++ InsuranceApp/InsuranceApp/DAL/InsuranceContext.cs
@@ -0,0 +1,23 @@
+﻿using InsuranceApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceApp.DAL
+{
+    public class InsuranceContext : DbContext
+    {
+        public InsuranceContext() : base("InsuranceContext") { }
+
+        public DbSet<Cars> Cars { get; set; }
+        public DbSet<Quotes> Quotes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
