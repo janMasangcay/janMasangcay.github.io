ec57568b771884155706ea1e90912c530e58d609
JanCaesar <janCaesar@github.com>
2019-05-18 23:32:03 -0700

    Created Cars and Quotes model.

---
+++ InsuranceApp/InsuranceApp/Models/Cars.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceApp.Models
+{
+    public class Cars
+    {
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ InsuranceApp/InsuranceApp/Models/Quotes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace InsuranceApp.Models
+{
+    public class Quotes
+    {
+        public string FirstName { get; set; }
+	    public string LastName { get; set; }
+	    public string EmailAddress { get; set; }
+	    public string DateOfBirth { get; set; }
+        public int CarYear { get; set; }
+	    public string CarMake { get; set; }
+        public string CarModel { get; set; }
+        public string DUI { get; set; }
+        public int SpeedingTicket { get; set; }
+	    public string Coverage { get; set; }
+        public double Amount { get; set; }
+    }
+}
